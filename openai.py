6519cd77e2d67171f66a3d75f781da08058f1598
Your Name <Your Email>
2024-03-12 14:05:55 +0900

    ss

--- openai.py
+++ openai.py
@@ -1,7 +1,21 @@
 from dotenv import load_dotenv
 import os
+from openai import OpenAI
 
 load_dotenv()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
+MODEL="gpt-3.5-turbo"
+client = OpenAI(api_key=openai_api_key)
+
+response = client.chat.completions.create(
+    model=MODEL,
+    messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+    ]
+)
+print(response.choices[0].message.content)
 pass
