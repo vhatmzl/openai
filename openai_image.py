dfc16edcd49b3bb9e1f3b344ded6dca48f9563ea
Your Name <Your Email>
2024-03-12 14:07:56 +0900

    qweas

---
+++ openai_image.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+import os
+from PIL import Image
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+MODEL="dall-e-3"
+
+client = OpenAI(api_key=openai_api_key)
+
+response = client.images.generate(
+    model=MODEL,
+    prompt="멋쟁이 사자를 그려줘",
+    size="1024x1024",
+    quality="standard",
+    n=1,
+)
+
+image_url = response.data[0].url
+
+# 저장 파일 이름 설정
+filename = 'image.jpg'
+response = requests.get(image_url)
+with open(filename, 'wb') as f:
+    f.write(response.content)
+Image.open(filename)
---
+++ openai_json.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+MODEL="gpt-3.5-turbo"
+client = OpenAI(api_key=openai_api_key)
+
+response = client.chat.completions.create(
+    model=MODEL,
+    response_format={ "type": "json_object" },
+    messages=[
+    {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+    {"role": "user", "content": "Who won the world series in 2020?"}
+  ]
+)
+print(response.choices[0].message.content)
+pass
